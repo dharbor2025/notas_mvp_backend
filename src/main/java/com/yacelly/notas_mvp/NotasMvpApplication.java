d72baed76f16b99a52692d2b1b18fa4af3761781
Jose Luis Yacelly <master.yac@gmail.com>
2025-01-25 18:15:31 -0400

    init(prject): Initial commit

---
+++ src/main/java/com/yacelly/notas_mvp/NotasMvpApplication.java
@@ -0,0 +1,13 @@
+package com.yacelly.notas_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotasMvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yacelly/notas_mvp/NotasMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yacelly.notas_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
